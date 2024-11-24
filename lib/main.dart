ce32ac560ace12b06f3cefb4d46e6828ee1d04ab
Chuong Cao <chuongb2105536@student.ctu.edu.vn>
2024-11-25 04:13:16 +0700

    Doi theme

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = ColorScheme.fromSeed(
+      seedColor: Colors.blue.shade900,
+      secondary: Colors.blue.shade600,
+      surface: Colors.white,
+      surfaceTint: Colors.blue.shade100,
+    );
+
+    final themeData = ThemeData(
+      fontFamily: 'Lato',
+      colorScheme: colorScheme,
+      useMaterial3: true,
+      appBarTheme: AppBarTheme(
+        backgroundColor: colorScheme.primary,
+        foregroundColor: colorScheme.onPrimary,
+        elevation: 4,
+        shadowColor: colorScheme.shadow,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'C-Housing',
+      debugShowCheckedModeBanner: false,
+      theme: themeData,
+      home: const MyHomePage(title: 'C-House'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text('Welcome to MyApp'),
+      ),
+    );
+  }
+}
